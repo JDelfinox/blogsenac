eb542c47d2dac733947860d238ffbabf5efc6103
JDelfinox <j,bieldelfis@gmail.com>
2024-08-12 21:17:36 -0300

    Falta muita coisa

--- Publication/urls.py
+++ Publication/urls.py
@@ -1,8 +1,10 @@
-from .views import index, about, postar
+from .views import index, about, entrar, cadastro
 from django.urls import path 
 
 urlpatterns = [
      path('', index, name='index'),
      path('about/', about, name='about'),
-     path('postar/', postar, name='postar')
+     path('entrar/', entrar, name='entrar'),
+     path('cadastro/', cadastro, name='cadastro')
+     
 ]
--- Publication/views.py
+++ Publication/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def postar(request):
-    return render(request, 'postar.html')
+def entrar(request):
+    return render(request, 'entrar.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
