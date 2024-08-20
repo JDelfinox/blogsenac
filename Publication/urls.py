0a15b22bf180a41f885103f300919c4146e631a7
JDelfinox <j,bieldelfis@gmail.com>
2024-08-19 21:34:22 -0300

    Front 100%

--- Publication/urls.py
+++ Publication/urls.py
@@ -1,10 +1,11 @@
-from .views import index, about, entrar, cadastro
+from .views import index, about, login, signup, logout, indexbase
 from django.urls import path 
 
 urlpatterns = [
      path('', index, name='index'),
      path('about/', about, name='about'),
-     path('entrar/', entrar, name='entrar'),
-     path('cadastro/', cadastro, name='cadastro')
-     
+    path('login/',login, name='login'),
+    path('signup/', signup, name='signup'),
+    path('logout/', logout, name='logout'),
+    path('indexbase/', indexbase, name='indexbase')
 ]
--- Publication/views.py
+++ Publication/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import logout as auth_logout
 
 def index(request):
     return render(request, 'index.html')
@@ -6,9 +9,32 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
-def entrar(request):
-    return render(request, 'entrar.html')
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redireciona para a página de login após o cadastro bem-sucedido
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
-def cadastro(request):
-    return render(request, 'cadastro.html')
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('indexbase')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
+
+def indexbase(request):
+    return render(request, 'indexbase.html')
+
+    
